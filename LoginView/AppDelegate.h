e3145fe782b53c6448eed5903a37f9b60e458716
Pham Thanh Binh <phamthanhbinh@mac11.local>
2017-05-05 15:35:46 +0700

    Initial Commit

---
+++ LoginView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginView
+//
+//  Created by Pham Thanh Binh on 5/5/17.
+//  Copyright © 2017 Pham Thanh Binh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginView
+//
+//  Created by Pham Thanh Binh on 5/5/17.
+//  Copyright © 2017 Pham Thanh Binh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
