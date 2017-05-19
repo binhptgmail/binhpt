e171267b37cd0db1013628ca2c7faa0ec6bd8b6d
Pham Thanh Binh <phamthanhbinh@mac11.local>
2017-05-19 15:45:12 +0700

    commit 1

---
+++ LoginView/DesignView.swift
@@ -0,0 +1,43 @@
+//
+//  DesignView.swift
+//  LoginView
+//
+//  Created by Pham Thanh Binh on 5/5/17.
+//  Copyright © 2017 Pham Thanh Binh. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class DesignView: UIView {
+    
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.CGColor
+        }
+    }
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
